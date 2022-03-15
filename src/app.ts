b3321085f510d9b186cecd48a655375648a2ef1b
igor <igorpeterssontrab@gmail.com>
2022-03-14 22:09:59 -0400

    feat: added error handiling model

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { routesApp } from "./routes";
+import { handleError } from "./utils/error";
 
 const app = express();
 
 routesApp(app)
+
+app.use((err: any, req: any, res: any, next: any) => {
+    handleError(err, res)
+})
 
 app.use(express.json())
 
